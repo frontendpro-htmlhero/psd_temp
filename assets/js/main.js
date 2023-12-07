23e63a5b1bf28e36f5a95908261e19cbd59a4965
frontendpro-htmlhero <maharasif80@gmail.com>
2023-12-07 18:47:08 +0500

    Style-update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,60 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+jQuery(document).ready(function ($) {
+  $(".lsp-owl").owlCarousel({
     items: 1,
     loop: true,
     autoPlay: true,
     nav: true,
-    // slideSpeed: 200,
+    center: true,
+    autoWidth:true,
+    slideBy:true,
     mouseDrag: true,
     autoplayHoverPause: true,
-    animateOut: 'slideOutDown',
-    responsiveClass: true,
+    responsiveClass:true,
     responsive: {
-      0: {
-        items: 1,
-
+      1024:{
+        items: 1
       },
-      600: {
-        items: 1,
-
+      768:{
+        items:1
       },
-      1000: {
-        items: 1,
-
+      425:{
+        items: 1
       }
-    }
+    } 
   });
+  $(".galley").owlCarousel({
+    items: 1,
+    loop: true,
+    autoPlay: true,
+    nav: true,
+    center: true,
+    autoWidth:true,
+    slideBy:true,
+    mouseDrag: true,
+    autoplayHoverPause: true,
+    responsiveClass:true,
+    responsive: {
+      1024:{
+        items: 1,
+        loop: true
+      },
+      768:{
+        items:1
+      },
+      425:{
+        items: 1
+      },
+      320:{
+        items: 1
+      }
+  } 
+  });
+  $("#hover_button").click(function(){
+    $(".main-menue").toggle();
+  })
+  Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+
+});
+
 });
